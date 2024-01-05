dabe41a2b2f8e9fe0d7de383c59eae59f228dbe4
Dennis <dennis.galinsky@gmail.com>
2024-01-05 12:23:54 -0500

    walking service

---
+++ src/components/booking/Walking/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Radio, Input } from 'antd';
+import { FormattedDatePicker } from '../../common/FormattedDatePicker';
+import { FormattedTimePicker } from '../../common/FormattedTimePicker';
+
+const styles = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+}
+
+export const WalkingService = () => {
+    return (
+        <>
+        <div style={styles}>
+            <FormattedDatePicker/>
+            <FormattedTimePicker/>
+        </div>
+        <div style={{paddingTop: "1em", width: "100%"}}>
+                <Radio.Group defaultValue="half" buttonStyle="solid">
+                    <Radio.Button value="half">Half Hour</Radio.Button>
+                    <Radio.Button value="hour">One Hour</Radio.Button>
+                </Radio.Group>
+        </div>
+        <p>Pick Up Address</p>
+        <Input placeholder="Pick Up Address"/>
+        </>
+    )
+}
