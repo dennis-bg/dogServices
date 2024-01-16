6738dd1e5771a601f79a645ff9decefed192182c
Dennis <dennis.galinsky@gmail.com>
2024-01-16 16:35:44 -0500

    overnight service

--- src/components/booking/OverNight/index.jsx
+++ src/components/booking/OverNight/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { FormattedDatePicker } from '../../common/FormattedDatePicker';
+import { FormattedTimePicker } from '../../common/FormattedTimePicker';
+import { LocationPicker } from '../../common/LocationPicker';
+
+const styles = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+}
 
 export const OvernightService = () => {
     return (
-        <p>Over night service</p>
+        <>
+            <p>Start Date & Time</p>
+            <div style={styles}>
+                <FormattedDatePicker range={false}/>
+                <FormattedTimePicker range={false}/>
+            </div>
+            <p>End Date & Time</p>
+            <div style={styles}>
+                <FormattedDatePicker range={false}/>
+                <FormattedTimePicker range={false}/>
+            </div>
+            <LocationPicker/>
+        </>
     )
 }
